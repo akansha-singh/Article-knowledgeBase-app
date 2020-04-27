97f02db5c9749cb62e2a7c2c1f082f8337af767a
akansha-singh <singhakansha281@gmail.com>
2020-04-27 20:36:56 +0530

    Mongoose and Model Setup

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+
+// Connect to database
+mongoose.connect('mongodb://localhost/nodekb');
+let db = mongoose.connection;
+
+// Check connection
+db.once('open', function(){
+    console.log('Connected to MongoDB');
+});
+
+// Check for DB errors
+db.on('error', function(err){
+    console.log(err);
+});
 
 // Init App
 const app = express();
+
+// Bring in Models
+let Article = require('./models/article');
+
 
 // Load View Engine
 app.set('views',path.join(__dirname, 'views'));
@@ -11,30 +30,16 @@ app.set('view engine', 'pug');
 
 // Home Route
 app.get('/', (req,res) => {
-    let articles = [
-        {
-            id:1,
-            title:'article One',
-            author:'John Smith',
-            body:'This is article one'
-        },
-        {
-            id:2,
-            title:'article Two',
-            author:'John Doe',
-            body:'This is article two'
-        },
-        {
-            id:3,
-            title:'article Three',
-            author:'John Smith',
-            body:'This is article three'
-        }
-    ];
-    res.render('index',{
+Article.find({}, function(err,articles){
+    if(err){
+        console.log(err);
+    } else {
+        res.render('index',{
         title: 'Articles',
         articles: articles
     });
+    }
+});
 });
 
 // Add Route
