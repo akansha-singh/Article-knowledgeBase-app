cecf8f4f252bedb01c854af18806cd2648d3d30b
akansha-singh <singhakansha281@gmail.com>
2020-04-27 12:07:29 +0530

    initial_commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req,res) => {
+    res.send('Hello World');
+});
+
+app.listen(3000, function(){
+    console.log('Server started at port 3000');
+});
