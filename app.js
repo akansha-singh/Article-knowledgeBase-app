72b9cc0bc926d0577097f0b61784859cfd1cc326
akansha-singh <singhakansha281@gmail.com>
2020-04-27 13:30:39 +0530

    Home route added

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const path = require('path');
 
+// Init App
 const app = express();
 
+// Load View Engine
+app.set('views',path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+
+// Home Route
 app.get('/', (req,res) => {
-    res.send('Hello World');
+    res.render('index',{
+        title: 'Articles'
+    });
 });
 
+// Start Server
 app.listen(3000, function(){
     console.log('Server started at port 3000');
 });
