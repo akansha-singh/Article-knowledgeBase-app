40c31ba249ffca2a9844a8242c76a5a4763d8967
akansha-singh <singhakansha281@gmail.com>
2020-04-29 12:30:34 +0530

    Save Articles to MongoDB, added Submit POST Route, body parser installed

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 // Connect to database
 mongoose.connect('mongodb://localhost/nodekb');
@@ -27,6 +28,14 @@ let Article = require('./models/article');
 app.set('views',path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// Body Parser Middleware
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+
 
 // Home Route
 app.get('/', (req,res) => {
@@ -48,6 +57,24 @@ app.get('/articles/add',function(req,res){
         title: 'Add Article'
 });
 });
+
+// Add Submit POST Route
+app.post('/articles/add/', function(req,res){
+    let article = new Article();
+    article.title = req.body.title;
+    article.author = req.body.author;
+    article.body = req.body.body;
+
+    article.save((err) => {
+        if(err){
+            console.log(err);
+            return;
+        } else {
+            res.redirect('/');
+        }
+    });
+});
+
 
 // Start Server
 app.listen(3000, function(){
