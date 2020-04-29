4de94bcedcd6dab26c4d89c012450ff9a6f9b67f
akansha-singh <singhakansha281@gmail.com>
2020-04-30 00:44:12 +0530

    Update Article route added

--- app.js
+++ app.js
@@ -97,6 +97,24 @@ app.get('/article/edit/:id',function(req,res){
     });
 });
 
+// Update Submit POST Route
+app.post('/articles/edit/:id', function(req,res){
+    let article = {};
+    article.title = req.body.title;
+    article.author = req.body.author;
+    article.body = req.body.body;
+    
+    let query = {_id:req.params.id}
+
+    Article.update(query, article, function(err){
+        if(err){
+            console.log(err);
+            return;
+        } else {
+            res.redirect('/');
+        }
+    });
+});
 
 // Start Server
 app.listen(3000, function(){
