f297794ebb2086cec708e09be47edcddffa92d6f
akansha-singh <singhakansha281@gmail.com>
2020-04-30 15:22:15 +0530

    Express Session Middleware,Messages Middleware,Validator Middleware

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+const flash = require('connect-flash');
+const session = require('express-session');
+
 
 // Connect to database
 mongoose.connect('mongodb://localhost/nodekb');
@@ -37,6 +41,38 @@ app.use(bodyParser.json());
 
 // Set Public Folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Express Session Middleware
+app.use(session({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true
+}));
+
+// Express Messages Middleware
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+// Express Validator Middleware
+app.use(expressValidator({
+    errorFormatter: function(param, msg, value) {
+        var namespace = param.split('.')
+        , root    = namespace.shift()
+        , formParam = root;
+  
+      while(namespace.length) {
+        formParam += '[' + namespace.shift() + ']';
+      }
+      return {
+        param : formParam,
+        msg   : msg,
+        value : value
+      };
+    }
+  }));
 
 
 // Home Route
@@ -81,6 +117,7 @@ app.post('/articles/add/', function(req,res){
             console.log(err);
             return;
         } else {
+            req.flash('success','Article Added');
             res.redirect('/');
         }
     });
