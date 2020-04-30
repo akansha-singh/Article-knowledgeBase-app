67c4d4f15be7a6752b6ba550212d9fcde4acfa62
akansha-singh <singhakansha281@gmail.com>
2020-04-30 20:13:34 +0530

    Added Validation

--- app.js
+++ app.js
@@ -105,8 +105,22 @@ app.get('/articles/add',function(req,res){
 });
 });
 
+
 // Add Submit POST Route
-app.post('/articles/add/', function(req,res){
+app.post('/articles/add', function(req,res){
+    req.checkBody('title','Title is required').notEmpty();
+    req.checkBody('author','Author is required').notEmpty();
+    req.checkBody('title','Body is required').notEmpty();
+
+// Get Errors
+let errors = req.validationErrors();
+
+if(errors){
+    res.render('add_article', {
+        title: 'Add Article',
+        errors:errors
+    });
+} else {
     let article = new Article();
     article.title = req.body.title;
     article.author = req.body.author;
@@ -121,6 +135,7 @@ app.post('/articles/add/', function(req,res){
             res.redirect('/');
         }
     });
+  }   
 });
 
 
